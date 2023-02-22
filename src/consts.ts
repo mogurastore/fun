3834788c9b576a8eae72c58d46b24d1673844305
lisp719 <368034+lisp719@users.noreply.github.com>
2023-02-22 15:33:31 +0000

    feat: Update site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "fun.mogura";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION = "ゲームやアプリの感想などを書きます。";
